01d8995415f0324d9291a76953243b25c6888828
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-13 12:44:04 -0300

    add: setting react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { View, Text, StatusBar } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import AuthRoutes from "./routes/auth.routes";
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="dark-content" backgroundColor="#fff" />
-    <View>
-      <Text>Olá</Text>
-    </View>
-  </>
+    <AuthRoutes />
+  </NavigationContainer>
 );
 
 export default App;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SignIn: React.FC = () => (
+  <View>
+    <Text>SignIn</Text>
+  </View>
+);
+
+export default SignIn;
