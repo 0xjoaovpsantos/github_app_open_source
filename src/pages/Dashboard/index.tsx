852cbfd7daaeec4d8f630f6cdd5958f745b9456e
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-06-16 02:26:52 -0300

    add: loader in dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { View, Text, Button, Image } from "react-native";
+import { View, Text, Button, Image, ActivityIndicator } from "react-native";
 
 import { ReposUser_URL } from "../../utils/urls";
 
@@ -33,10 +33,12 @@ const Dashboard: React.FC = () => {
   const { signOut, codeAccess } = useAuth();
   const [allRepos, setAllRepos] = useState<ReposProps[]>([]);
   const [searchRepos, setSearchRepos] = useState<ReposProps[]>();
+  const [loading, setLoading] = useState(false);
 
   const navigation = useNavigation();
 
   const getRepos = useCallback(async () => {
+    setLoading(true);
     try {
       const instance = axios.create({
         headers: {
@@ -55,6 +57,7 @@ const Dashboard: React.FC = () => {
     } catch (err) {
       console.log(err);
     }
+    setLoading(false);
   }, []);
 
   const updateListRepos = useCallback((text) => {
@@ -87,32 +90,40 @@ const Dashboard: React.FC = () => {
           onChangeText={(text) => updateListRepos(text)}
         ></SearchInput>
       </ContainerSearchInput>
-      <FlatList
-        style={{ flex: 1 }}
-        data={searchRepos}
-        keyExtractor={(repository) => repository.id.toString()}
-        renderItem={({ item: repository }) => (
-          <ContainerRepository
-            onStartShouldSetResponder={() => {
-              navigation.navigate("Commits", {
-                name: repository.name,
-                user: repository.owner.login,
-              });
-              return true;
-            }}
-          >
-            <RepositoryName>{repository.name}</RepositoryName>
-            <ContainerRepositoryDescription>
-              <RepositoryDescription>
-                {repository.description != null
-                  ? repository.description
-                  : "Not have description"}
-              </RepositoryDescription>
-            </ContainerRepositoryDescription>
-            <Underline />
-          </ContainerRepository>
-        )}
-      ></FlatList>
+      {loading ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator size="large" color="#999" />
+        </View>
+      ) : (
+        <FlatList
+          style={{ flex: 1 }}
+          data={searchRepos}
+          keyExtractor={(repository) => repository.id.toString()}
+          renderItem={({ item: repository }) => (
+            <ContainerRepository
+              onStartShouldSetResponder={() => {
+                navigation.navigate("Commits", {
+                  name: repository.name,
+                  user: repository.owner.login,
+                });
+                return true;
+              }}
+            >
+              <RepositoryName>{repository.name}</RepositoryName>
+              <ContainerRepositoryDescription>
+                <RepositoryDescription>
+                  {repository.description != null
+                    ? repository.description
+                    : "Not have description"}
+                </RepositoryDescription>
+              </ContainerRepositoryDescription>
+              <Underline />
+            </ContainerRepository>
+          )}
+        ></FlatList>
+      )}
       <TouchableHighlight onPress={() => signOut()} style={{ marginTop: 20 }}>
         <Text>Logout</Text>
       </TouchableHighlight>
